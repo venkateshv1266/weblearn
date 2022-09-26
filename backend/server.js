9533db26e5efc083810e85f15259d3725184d974
Venkatesh V <88155163+venkateshv1266@users.noreply.github.com>
2022-09-27 00:58:46 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -35,10 +35,6 @@ app.get('/api/data', (req,res) => {
     res.send(data);
 });
 
-app.get('/', (req, res) => {
-    res.status(200).send('Server Connected');
-});
-
 //to serve the frontend files
 app.use(express.static(path.join(__dirname, '../frontend/build'))); 
 app.get('*', (req,res) => res.sendFile(path.join(__dirname, '../frontend/build/index.html')));
